7f85ce74e51a6367bd0d8645ccd3b8c150992fd4
vn-vision <vctrngang@gmail.com>
2024-04-16 04:01:52 -0700

    filter all, delete some

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,15 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls:
+            filtered = {}
+            for key, value in self.__objects.items():
+                if type(value) == cls:
+                    filtered[key] = value
+            return filtered
+        return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -45,6 +51,21 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """
+        Deletes the obj from __objects if it exists
+        returns if the obj is not passed
+        """
+
+        if obj is None:
+            return
+
+        # delete key
+        dl_key = "{}.{}".format(obj.__class__.__name__, obj.id)
+
+        if dl_key in self.__objects:
+            del self.__objects[dl_key]
