d886d4caf846f2c8008f0a13d166b3cd83b31c73
Karushi536 <karushiabigael@gmail.com>
2024-04-17 22:49:31 +0300

    no 9

--- models/user.py
+++ models/user.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-"""User class"""
+"""This is the user class"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, DateTime, Column, ForeignKey
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
+
 
 class User(BaseModel, Base):
     """This is the class for user
@@ -12,12 +16,13 @@ class User(BaseModel, Base):
         first_name: first name
         last_name: last name
     """
-    __tablename__ = 'users'
-
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship('Place', backref='user', cascade='all, delete-orphan')
-    reviews = relationship('Review', backref='user', cascade='all, delete-orphan')
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user")
+    reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                           backref="user")
      
