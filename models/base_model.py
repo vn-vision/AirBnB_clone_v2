38d8d4e577c15f571de09640eceae2e3f14b5235
vn-vision <vctrngang@gmail.com>
2024-04-17 11:49:42 +0300

    State naybe and some changes to basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -24,11 +24,11 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-        else:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        
+        if kwargs:
             for key, value in kwargs.items():
                 # check if the class has an attribute matching key
                 if hasattr(self, key):
