93b571e5546297730f2dc89f4535d29d720fdb50
vn-vision <vctrngang@gmail.com>
2024-04-18 11:58:50 +0300

    Creating table USER

--- models/user.py
+++ models/user.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 
-
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+
+    # create table user
+    __tablename__ = 'users'
+    
+    # Add the different attributes of the user, columns
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
