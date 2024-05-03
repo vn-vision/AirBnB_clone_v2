5fa468380d561af261e1c4d9b38a2d5c3a72d19a
vn-vision <vctrngang@gmail.com>
2024-05-03 17:43:50 +0300

    Maybe simple is better

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,69 +1,12 @@
 #!/usr/bin/env bash
-# this script sets up web servers for deployment of web_static
+# sets up the web servers for the deployment of web_static
 
-# check if nginx is installed before trying to install
-if ! command -v nginx &> /dev/null
-then
-	apt-get install nginx -y
-fi
-
-# create different folders if they don't exist
-if [ ! -d /data/ ]
-then
-	mkdir /data/
-fi
-
-if [ ! -d /data/web_static/ ]
-then
-		mkdir /data/web_static/
-fi
-
-if [ ! -d /data/web_static/releases/ ]
-then
-	mkdir /data/web_static/releases/
-fi
-
-if [ ! -d /data/web_static/releases/test/ ]
-then
-		mkdir /data/web_static/releases/test/
-fi
-
-if [ ! -f /data/web_static/releases/test/index.html ]
-then
-	echo "This is tedious" > /data/web_static/releases/test/index.html
-fi
-
-# create symbolic link and recreate it every time the script is run
-the_link="/data/web_static/current"
-if [ -L "$the_link" ]
-then
-	rm "$the_link"
-fi
-
-# recreate it
-ln -s "/data/web_static/releases/test/" "$the_link"
-
-# change ownership of the data folder recursively
-chown -R ubuntu:ubuntu /data/
-
-# Define the paths and configuration file
-nginx_config="/etc/nginx/sites-available/default"
-web_static_dir="/data/web_static/current"
-
-# Define the new location block
-location_block="location /hbnb_static {
-    alias $web_static_dir;
-}"
-
-# Check if the location block already exists in the configuration file
-if grep -q "$location_block" "$nginx_config"; then
-    echo "Location block already exists. Skipping..."
-else
-    # Add the location block to the configuration file using sed
-    sudo sed -i "/server_name _;/a $location_block" "$nginx_config"
-    echo "Location block added."
-fi
-
-# Restart Nginx
-sudo systemctl restart nginx
-
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -hR ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo service nginx start
