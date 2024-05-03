88f120eacded3ec26493ca34da6bf44a4a51a444
vn-vision <vctrngang@gmail.com>
2024-05-03 17:53:08 +0300

    Back to starting point with this one

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,49 +1,44 @@
 #!/usr/bin/python3
-# Fabfile to distribute an archive to a web server.
-import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+"""
+Fabric script to distribute an archive to web servers
+"""
 
-env.hosts = ["104.196.168.90", "35.196.46.172"]
+from fabric.api import env, put, run
+from os.path import exists
+
+env.hosts = ['54.174.207.67', '54.210.123.94']
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to a web server.
-
-    Args:
-        archive_path (str): The path of the archive to distribute.
-    Returns:
-        If the file doesn't exist at archive_path or an error occurs - False.
-        Otherwise - True.
     """
-    if os.path.isfile(archive_path) is False:
-        return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
+    Distributes an archive to web servers
+    """
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    if not exists(archive_path):
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
+
+    try:
+        archive_name = archive_path.split('/')[-1]
+        folder_name = '/data/web_static/releases/{}'.format(
+            archive_name.split('.')[0])
+
+        # Upload archive to /tmp/ directory of web servers
+        put(archive_path, '/tmp/')
+
+        # Uncompress archive to /data/web_static/releases/<archive filename without extension>
+        run('mkdir -p {}'.format(folder_name))
+        run('tar -xzf /tmp/{} -C {}'.format(archive_name, folder_name))
+
+        # Delete the archive from the web servers
+        run('rm /tmp/{}'.format(archive_name))
+
+        # Delete symbolic link /data/web_static/current
+        run('rm -rf /data/web_static/current')
+
+        # Create new symbolic link /data/web_static/current linked to the new version
+        run('ln -s {} /data/web_static/current'.format(folder_name))
+
+        return True
+    except:
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
-    return True
+
