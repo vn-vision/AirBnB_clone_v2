7ff5aa175af72c13621e3285fc766c11f7d275f4
Karushi536 <karushiabigael@gmail.com>
2024-05-23 00:28:23 +0300

    ilc

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
-# sets up the web servers for the deployment of web_static
+# Sets up a web server for deployment of web_static.
 
-# Update and upgrade the system
-sudo apt-get -y update
-sudo apt-get -y full-upgrade
-
-if! dpkg -s mginx &> /dev/null: then
-sudo apt-get -y install nginx
-fi
-
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install nginx -y
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx restart
+sudo service nginx start
--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""
-Fabric script that generates a tgz archive from the contents of the web_static
-folder of the AirBnB Clone repo
-"""
+"""Module>>Fabric script that generates a .tgz archive"""
 
 from datetime import datetime
 from fabric.api import local
-from os.path import isdir, join, abspath
+from os.path import isdir
 
 
 def do_pack():
     """generates a tgz archive"""
-    date = datetime.now().strftime("%Y%m%d%H%M%S")
-    versions_folder = "versions"
-    archive_name = f"web_static_{date}.tgz"
-    archive_path = join(versions_folder, archive_name)
-
-    if isdir("versions_folder") is False:
-        local("mkdir -p{}". versions)
-    file_name = "versions/web_static_{}.tgz".format(date)
-    local("tar -cvzf {} web_static".format(file_name))
-    return file_name
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
